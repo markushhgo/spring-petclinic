c67b4b762d48aea1be38f53fa1f73d487f6c2b82
Markus Hällfors <markus.hallfors@gofore.com>
2020-06-15 15:08:21 +0300

    Update terraform

--- main.tf
+++ main.tf
@@ -13,8 +13,27 @@ provider "azurerm" {
   features {}
 } 
 
-# Create a resource group
-resource "azurerm_resource_group" "markushResourceGroup" {
-  name     = "markushResourceGroup"
-  location = "West Europe"
+resource "azurerm_container_group" "petcliniccontainer" {
+  name                = "petclinic-continst"
+  location            = "West Europe"
+  resource_group_name = "markushResourceGroup"
+  ip_address_type     = "public"
+  dns_name_label      = "aci-labell"
+  os_type             = "Linux"
+
+  container {
+    name   = "petclinic"
+    image  = "markushtesti.azurecr.io/petclinic:latest"
+    cpu    = "0.5"
+    memory = "1.5"
+
+    ports {
+      port     = 80
+      protocol = "TCP"
+    }
+  }
+
+  tags = {
+    environment = "testing"
+  }
 }
